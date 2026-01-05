2a84c3299b515f4633458388a8a8ca8bcaea0a6a
kostryginden <kostryginden@gmail.com>
2026-01-05 17:46:50 +0300

    feat: добавить обработку загрузки меню  del-1

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -9,13 +9,24 @@ import styles from './styles.module.css';
 
 export function Menu() {
         const [products, setProducts] = useState<Product[]>([]);
+        const [isLoading, setIsLoading] = useState<boolean>(false);
+
+        console.log('isLoading', isLoading);
 
         const getMenu = async () => {
                 try {
+                        setIsLoading(true);
+                        await new Promise<void>(resolve => {
+                                setTimeout(() => {
+                                        resolve();
+                                }, 2000);
+                        });
                         const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
                         setProducts(data);
+                        setIsLoading(false);
                 } catch (e) {
                         console.error(e);
+                        setIsLoading(false);
                         return;
                 }
         };
@@ -31,17 +42,19 @@ export function Menu() {
                                 <Search placeholder='Введите блюдо или состав' />
                         </div>
                         <div className={styles['products']}>
-                                {products.map(product => (
-                                        <ProductCard
-                                                key={product.id}
-                                                id={product.id}
-                                                title={product.name}
-                                                description={product.ingredients.join(', ')}
-                                                image={product.image}
-                                                price={product.price}
-                                                rating={product.rating}
-                                        />
-                                ))}
+                                {isLoading && 'Загрузка меню...'}
+                                {!isLoading &&
+                                        products.map(product => (
+                                                <ProductCard
+                                                        key={product.id}
+                                                        id={product.id}
+                                                        title={product.name}
+                                                        description={product.ingredients.join(', ')}
+                                                        image={product.image}
+                                                        price={product.price}
+                                                        rating={product.rating}
+                                                />
+                                        ))}
                         </div>
                 </>
         );
